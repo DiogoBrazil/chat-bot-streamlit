9dfc71fc5da930db3b412b6b0ac8e1e625cf20e7
Diogo Ribeiro <diogoifroads@gmail.com>
2023-12-03 10:22:58 -0400

    remove style in the syle.css file

--- modules/openai_api.py
+++ modules/openai_api.py
@@ -15,7 +15,7 @@ def get_openai_response(prompt):
     )
 
     full_response = response.choices[0].text.strip()
-    if full_response.startswith(("bot:", "assistant:", "Robô:", "Atendente:", "Resposta:")):
+    if full_response.startswith(("Bot:", "bot:", "assistant:", "Robô:", "robô:", "Atendente:", "Resposta:")):
         full_response = full_response.split(":", 1)[1].strip()
 
     return full_response
